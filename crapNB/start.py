97ff7d0517d372d7b818ae84a48f7e4a98e0e89d
vit <viktorr1975@yandex.com>
2023-01-25 00:36:41 +0700

    небольшие доработки запуска и описания проекта

--- crapNB/start.py
+++ crapNB/start.py
@@ -1,5 +1,6 @@
 from scrapy import cmdline
 import sys
+import os
 from scrapy.crawler import CrawlerProcess
 from scrapy.utils.project import get_project_settings
 #from crapNB.spiders import firstspider
